70bfc9114913b5cca070f4ef20c059cd34864f0b
Victor Hugo Faria <victorfaria1590@gmail.com>
2019-10-18 12:23:22 -0300

    SIN142IOBound - Corrigindo identação.

--- src/SIN142IOBound/SIN142IOBound.java
+++ src/SIN142IOBound/SIN142IOBound.java
@@ -2,9 +2,9 @@
  * Main
  */
 
- //PS: Clonar diretório e fazer commit apenas da pasta /src.
- //Crie um projeto SIN-142-Projeto-Avaliativo e um package chamado SIN142IOBound para rodar o código abaixo.
- 
+//PS: Clonar diretório e fazer commit apenas da pasta /src.
+//Crie um projeto SIN-142-Projeto-Avaliativo e um package chamado SIN142IOBound para rodar o código abaixo.
+
 package SIN142IOBound;
 
 /**
@@ -17,36 +17,36 @@ import java.io.*;
 
 public class SIN142IOBound {
 	/*Um método para ler arquivos (ainda não implementado).
-	*public static ArrayList<Integer> lerArquivo(String path) {
-	*	return 0
-	*}
-	*/
-	
+	 *public static ArrayList<Integer> lerArquivo(String path) {
+	 *	return 0
+	 *}
+	 */
+
 	/*
-	* Método para criar diretório /arquivos/ na pasta do programa.
-	* Retorno: string com o caminho da pasta.
-	*/
+	 * Método para criar diretório /arquivos/ na pasta do programa.
+	 * Retorno: string com o caminho da pasta.
+	 */
 	public static String criarDiretorio() {
 		// Pega endereço do aplicativo.
 		String path = System.getProperty("user.dir");
 		// Cria diretório em path + /arquivos/. Path = pasta do projeto.
 		File dir = new File(path + "/arquivos/");
 		if (!dir.exists()) {
-            if (dir.mkdir()) {
-            System.out.println("Diretório /arquivos/ criado.");
-            return path + "/arquivos/";
-            } 
-            else {
-            System.out.println("Falha para criar o diretório.");
-            return path;
-            }
+			if (dir.mkdir()) {
+				System.out.println("Diretório /arquivos/ criado.");
+				return path + "/arquivos/";
+			} 
+			else {
+				System.out.println("Falha para criar o diretório.");
+				return path;
+			}
 		}
 		else {
 			System.out.println("Diretório já existe!");
 			return path + "/arquivos/";
 		}
 	}
-	
+
 	/*
 	 * Método que cria um arquivo com um nome numérico.
 	 * num_arquivo é o parâmetro que define como se chamará o arquivo.
@@ -60,34 +60,34 @@ public class SIN142IOBound {
 			System.out.printf("\nArquivo %d.txt criado.", num_arquivo);
 			return file;
 		}catch(IOException e) {
-		    System.out.printf("Arquivo %d.txt não pôde ser criado", num_arquivo);
-		    return null;
+			System.out.printf("Arquivo %d.txt não pôde ser criado", num_arquivo);
+			return null;
 		}
-		
+
 		//Trecho alternativo não utilizado.
-		
-//		if (file.createNewFile()){
-//		    System.out.printf("\nArquivo {0}.txt criado.", num_arquivo);
-//		    return file;
-//		}
-//		else {
-//		    System.out.printf("Arquivo {0}.txt já existe.", num_arquivo);
-//		    return file;
-//		}
+
+		//		if (file.createNewFile()){
+		//		    System.out.printf("\nArquivo {0}.txt criado.", num_arquivo);
+		//		    return file;
+		//		}
+		//		else {
+		//		    System.out.printf("Arquivo {0}.txt já existe.", num_arquivo);
+		//		    return file;
+		//		}
 	}//fim método criarArquivo.
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		//System.out.printf("Hello World");
 		//ArrayList<Integer> lista = new ArrayList<Integer>();
-		
+
 		//Testando método criarDiretorio.
 		File file;
 		int i;
 		String path = criarDiretorio();
-		
+
 		/*
 		 * Teste: Criando 10 arquivos com nome {número de 0 a 10}.txt
 		 * e escrevendo nestes arquivos o número de 0 a 10 correspondente ao
