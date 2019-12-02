2245d8cd67da086320cef7afb12413d5f6cba571
Victor Hugo Faria <victorfaria1590@gmail.com>
2019-12-02 19:37:17 -0300

    Código modificado

--- gerenciarrecursos/LeitorEscritor.java
+++ gerenciarrecursos/LeitorEscritor.java
@@ -1,102 +1,58 @@
 package gerenciarrecursos;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.concurrent.*; 
+import java.util.concurrent.Semaphore;
+
+import sun.applet.Main;
 
 // RED LEITOR
 
 // GREEN ESCRITOR
 public class LeitorEscritor extends Thread{
-    private boolean colorChanged, stop, reader; 
-    private ColorSemaphore color;
-    private String name;
-    int leitor, escritor;
-
+        
     private int views, likes, dislikes, iViews, iLikes, iDislikes; // variaveis auxiliares
     Video v1 = new Video();
-    
+    Semaphore sem;
     public LeitorEscritor(){}
-    public LeitorEscritor(String name){
-        super(name);
-        this.name = name;
-        this.color = ColorSemaphore.RED;
-    }
     
-    @Override
+    
+    public LeitorEscritor(Semaphore sem) {
+		super();
+		this.sem = sem;
+	}
+
+
+	@Override
     public void run(){
-        while(!stop){
-            try {
-                switch(this.color){
-                    case RED:
-                        if(reader){
-                        Thread.sleep(70);
-                            System.out.println("Lendo valores...");
-                            System.out.printf("Valores lidos: \nViews:%d Likes: %d Dislikes: %d\n", views, likes, dislikes);
-                            System.out.printf("Valores invalidos: \nViews:%d Likes: %d Dislikes: %d\n", iViews, iLikes, iDislikes);   
-                        }
-                        this.setReader(false); // acabou de ler
-                        break;
-                    case GREEN:
-                        if(!reader){
-                            Thread.sleep(70);
-                            System.out.println("Escrevendo valores...");
-                            v1.addViews(views);
-                            v1.removeViews(iViews);
-                            v1.addLikes(likes);
-                            v1.removeLikes(iLikes);
-                            v1.addDislikes(dislikes);
-                            v1.removeDislikes(iDislikes);
-                        }
-                        this.setReader(true); // inicia leitura
-                        break;
-                }
-                this.changeColor();
-            }catch(InterruptedException ex){
-                System.out.println("%s"+ ex);
-            }
-        }  
-    }
-    
-    public synchronized void setReader(boolean reader){
-        this.reader = reader;
-    }
-    
-    public boolean getReader(){
-        return this.reader;
-    }
-    
-    public synchronized void stopSemaphoro(){
-        this.stop = true;
-    }
-    
-    private synchronized void changeColor(){
-        switch(this.color){
-            case RED:
-                this.color = ColorSemaphore.GREEN;
-                break;
-            case GREEN:
-                this.color = ColorSemaphore.RED;
-                break;
-        }
-        this.colorChanged = true;
-        notify();
-    }
-    
-    public synchronized void waitColorChange(){
-        while(!this.colorChanged){
-            try {
-                wait();
-            } catch (InterruptedException ex) {
-                System.out.println("%s"+ ex);
-            }
-        }
-        this.colorChanged = false;
-    }
-    
-    public ColorSemaphore getColor(){
-        return color;
-    }
+		while(GerenciarRecursos.scanner.hasNext() && GerenciarRecursos.scanner2.hasNext()) {
+			try {
+				if(GerenciarRecursos.scanner.hasNext() && GerenciarRecursos.scanner2.hasNext()) {
+					sem.acquire();
+					this.addViews(GerenciarRecursos.scanner.nextInt());
+			        this.removeViews(GerenciarRecursos.scanner2.nextInt());
+			        this.addLikes(GerenciarRecursos.scanner.nextInt());
+			        this.removeLikes(GerenciarRecursos.scanner2.nextInt());
+			        this.addDislikes(GerenciarRecursos.scanner.nextInt());
+			        this.removeDislikes(GerenciarRecursos.scanner2.nextInt());        
+//					System.out.println("Lendo valores...");
+			        System.out.printf("%d|%d|%d\n", views, likes, dislikes);
+//			        System.out.printf("Valores lidos: Views:%d Likes: %d Dislikes: %d\n", views, likes, dislikes);
+//			        System.out.printf("Valores invalidos: \nViews:%d Likes: %d Dislikes: %d\n", iViews, iLikes, iDislikes);
+//			        System.out.println("Escrevendo valores...");
+			        v1.addViews(views);
+			        v1.removeViews(iViews);
+			        v1.addLikes(likes);
+			        v1.removeLikes(iLikes);
+			        v1.addDislikes(dislikes);
+			        v1.removeDislikes(iDislikes);
+				}//if
+			}catch(InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+			sem.release();
+		}//while
+	}
+      
     
     public synchronized void removeLikes(int likes){
         this.iLikes = likes;
